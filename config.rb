bf250c8c4d14254aad1651063d153af838e7af09
lofic <louis.coilliot@gmail.com>
2012-05-11 14:40:13 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/kermit/"
+http_images_path = "/kermit/images"
+http_fonts_path = "/kermit/fonts"
+css_dir = "public/kermit/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
---
+++ source/screencasts/index.markdown
@@ -0,0 +1,10 @@
+---
+layout: page
+title: "screencasts"
+date: 2012-05-10 14:20
+comments: true
+sharing: true
+footer: true
+---
+
+Lofic was here.
